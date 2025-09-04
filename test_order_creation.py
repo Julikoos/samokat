d05f38ebc56cdd01d3a905108a236ad4913566ea
Julia Kosik <kosik.yulya02@mail.ru>
2025-09-04 22:45:30 +0300

    Initial_commit

--- test_order_creation.py
+++ test_order_creation.py
@@ -2,6 +2,7 @@ import data
 from sender_samokat_request import post_new_order, get_order_by_track
 
 
+# Косик Юлия, 34-я когорта — Финальный проект. Инженер по тестированию плюс
 def test_create_order_and_get_by_track():
     response = post_new_order(data.new_order_body)
     assert response.status_code == 201
